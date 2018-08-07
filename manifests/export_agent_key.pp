c731cf54a5931a4303e99fb0104d90540f54337c
Dan Fuhry <dan@fuhry.com>
2018-08-07 23:20:09 +0000

    More bulletproof foreman search queries

--- manifests/export_agent_key.pp
+++ manifests/export_agent_key.pp
@@ -9,10 +9,20 @@ class wazuh::export_agent_key(
     $host_info = foreman({
       foreman_user => $::foreman_api_user,
       foreman_pass => $::foreman_api_password,
-      item         => "v2/hosts/${::fqdn}",
+      item         => "v2/hosts",
+      search       => "(name = \"$::hostname\" or name = \"$::fqdn\") and last_report > \"1 day ago\"",
+      per_page     => 1,
     })
 
-    $agent_id = $host_info['id']
+    if $host_info['results'][0]['name'] == $::hostname or
+       $host_info['results'][0]['name'] == $::fqdn
+    {
+      $agent_id = $host_info['results'][0]['id']
+    }
+    else {
+      notify { "Failed to identify this host in a Foreman search. Using unsafe (collision-prone) fqdn_rand() to generate agent_id": }
+      $agent_id = fqdn_rand($max_clients)
+    }
   }
   else {
     notify { "Using unsafe (collision-prone) fqdn_rand() to generate agent_id": }
